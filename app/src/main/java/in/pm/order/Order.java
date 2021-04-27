56145a427d40d8be4c950b0909b856b86f1564cd
SS <majhi9442@gmail.com>
2021-04-28 04:53:39 +0530

    Initial Commit

---
+++ app/src/main/java/in/pm/order/Order.java
@@ -0,0 +1,15 @@
+package in.pm.order;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Order extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order);
+
+            }
+}
